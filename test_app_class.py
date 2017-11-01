4dfa9c11d590b2241b69214029725137a85a2abe
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2017-11-01 06:57:56 +0300

    Add class definitions in app_class.py and tests in test_app_class.py
    Added definitions of classes in app_class.py. The Recipes class is still
    a work in progress thus the pass statements and a lack of any actual code
    there.

---
+++ test_app_class.py
@@ -0,0 +1,59 @@
+"""Unittests for the User and Recipe classes"""
+from unittest import TestCase
+from app_class import Recipe, User
+
+class UserTestCase(TestCase):
+    """This is an instance of the Test Case class
+    that will be used in testing the User class in
+    app_class.py"""
+    def setUp(self):
+        self.user = User()
+
+    def test_register_user(self):
+        """A method to test the accuracy of the
+        register_user method in the User class"""
+        email = "iseeyou@xyz.com"
+        password = "somepassword"
+        confirm_password = "somepassword"
+        self.user.register_user(email, password, confirm_password)
+        self.assertTrue(email in self.user.user_db, msg="No e-mail address entered")
+        self.assertTrue(self.user.user_db[email], msg="No password given")
+        self.assertEqual(self.user.user_db[email], password,
+                         msg='User e-mail address and password not successfully associated')
+        self.assertIn(email, self.user.user_db,
+                      msg='E-mail address not added to user entries')
+
+    def test_login_user(self):
+        """A method to test the accuracy of the
+        login_user method in the User class"""
+        email = "iseeyou@xyz.com"
+        password = "somepassword"
+        self.user.user_db.update({email: password})
+        self.assertEqual(self.user.user_db[email], password, msg='Invalid user password')
+
+    class RecipeTestCase(TestCase):
+        """This is an instance of the Test Case class
+        that will be used in testing the Recipe class
+        in app_class.py"""
+        def setUp(self):
+            self.my_recipe = Recipe()
+
+        def test_create_recipe_category(self):
+            """A method to test the create_recipe_category method
+            in the Recipe class"""
+            pass
+
+        def test_delete_recipe_category(self):
+            """A method to test the delete_recipe_category
+            method in the Recipe class"""
+            pass
+
+        def test_update_recipe_category(self):
+            """A method to test the update_recipe_category method
+            in the BucketList class"""
+            pass
+
+        def test_view_recipe_category(self):
+            """A method to test the view_recipe_category method
+            in the BucketList class"""
+            pass
